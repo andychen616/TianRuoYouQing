a7a1eabb7b920f20b204ab7c128f824c618ea03f
andychen616 <156666676+andychen616@users.noreply.github.com>
2026-05-07 15:46:09 +0800

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,14 @@ import Home from '../views/Home.vue'
 import SearchResults from '../views/SearchResults.vue'
 import About from '../views/About.vue'
 import Settings from '../views/Settings.vue'
+import Message from '../views/Message.vue'
 
 const routes = [
   { path: '/', component: Home },
   { path: '/search', component: SearchResults },
   { path: '/about', component: About },
   { path: '/settings', component: Settings },
+  { path: '/message', component: Message },
 ]
 
 const router = createRouter({
